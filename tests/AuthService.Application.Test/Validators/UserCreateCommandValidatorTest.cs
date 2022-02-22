9d261790f4343129a490a53047145e4f169ca230
Rodrigo Vaz <rodrigodp2014@gmail.com>
2022-02-21 21:51:50 -0300

    test: add tests for validation classes

---
+++ tests/AuthService.Application.Test/Validators/UserCreateCommandValidatorTest.cs
@@ -0,0 +1,78 @@
+﻿using AuthService.Application.Core.Users.Commands;
+using AuthService.Application.Core.Users.Validators;
+using Bogus;
+using FluentAssertions;
+using Xunit;
+
+namespace AuthService.Application.Test.Validators
+{
+    public class UserCreateCommandValidatorTest
+    {
+        private static readonly Faker faker = new("en");
+        private static string _password = faker.Lorem.Letter(10);
+        private string Email = faker.Internet.Email();
+        private string Password = _password;
+        private string ConfirmPassword = _password;
+
+        private UserCreateCommandValidator _validator;
+        private UserCreateCommand _userCreateCommand;
+
+        public UserCreateCommandValidatorTest()
+        {
+            _validator = new UserCreateCommandValidator();
+            _userCreateCommand = new UserCreateCommand
+            {
+                Email = Email,
+                Password = Password,
+                ConfirmPassword = ConfirmPassword,
+            };
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("exemple")]
+        [InlineData("exemple@")]
+        [InlineData("@exemple")]
+        [Trait("Application", "Identity User")]
+        public void Should_Not_Create_Identity_User_With_Invalid_UserName(string invalidEmail)
+        {
+            _userCreateCommand.Email = invalidEmail;
+
+            var result = _validator.ValidateAsync(_userCreateCommand).Result;
+
+            result.IsValid.Should().BeFalse();
+            result.Errors?.Should().NotBeEmpty();
+            result.Errors?.Should().HaveCount(c => c > 0).And.OnlyHaveUniqueItems();
+            result.Errors?.Should().Contain(x => x.ErrorMessage.Contains("Email"));
+        }
+
+        [Fact]
+        [Trait("Application", "Identity User")]
+        public void Should_Not_Create_Identity_User_With_Invalid_Password()
+        {
+            _userCreateCommand.Password = string.Empty;
+
+            var result = _validator.ValidateAsync(_userCreateCommand).Result;
+
+            result.IsValid.Should().BeFalse();
+            result.Errors?.Should().NotBeEmpty();
+            result.Errors?.Should().HaveCount(c => c > 0).And.OnlyHaveUniqueItems();
+            result.Errors?.Should().Contain(x => x.ErrorMessage.Contains("Password"));
+        }
+
+        [Fact]
+        [Trait("Application", "Identity User")]
+        public void Should_Not_Create_Identity_User_With_Diferent_Passwords()
+        {
+            _userCreateCommand.ConfirmPassword = faker.Lorem.Letter(2);
+
+            var result = _validator.ValidateAsync(_userCreateCommand).Result;
+
+            result.IsValid.Should().BeFalse();
+            result.Errors?.Should().NotBeEmpty();
+            result.Errors?.Should().HaveCount(c => c > 0).And.OnlyHaveUniqueItems();
+            result.Errors?.Should().Contain(x => x.ErrorMessage.Contains("don't match"));
+        }
+    }
+}
